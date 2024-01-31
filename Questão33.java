1b004a2f268adcdf06e0b18e5a2c24d329c1c877
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-01-31 01:26:42 -0300

    Questão 33: Contador de letras com HashMap

---
+++ Questão33.java
@@ -0,0 +1,26 @@
+    import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+    public class Questão33 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Digite a palavra a ser contada: ");
+            String palavra = sc.nextLine();
+
+            Map<Character, Integer> contadorLetras = new HashMap<>();
+
+            for (char letra : palavra.toCharArray()) {
+                if (Character.isLetter(letra)) {
+                   contadorLetras.put(letra, contadorLetras.getOrDefault(letra, 0) + 1);
+                   //System.out.println(contadorLetras.get(letra));
+                }
+            }
+
+            System.out.println("Resultado:");
+            for (Map.Entry<Character, Integer> entry : contadorLetras.entrySet()) {
+                System.out.println(entry.getKey() + " = " + entry.getValue());
+            }
+            sc.close();
+        }
+    }
