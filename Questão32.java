3e2f0d3685bf846751e0061c70d5f0813e27f995
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-01-31 01:26:15 -0300

    Questão 32 e seus recursos

---
+++ Questão32.java
@@ -0,0 +1,80 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Questão32 {
+    private static String FILE_NAME = "pessoas.bin";
+
+    public static void main(String args[]){
+
+        List<Pessoa> pessoas = carregarDoArquivo();
+        exibirPessoas(pessoas);
+
+        System.out.println("Quantas pessoas deseja criar?");
+        Scanner sc = new Scanner(System.in);
+
+        int qtd = sc.nextInt();
+
+        for (int i = 0; i < qtd; i++) {
+            System.out.println("Informe os dados para a pessoa " + (i + 1));
+            System.out.print("CPF: ");
+            String cpf = sc.next();
+            System.out.print("Nome: ");
+            sc.nextLine();
+            String nome = sc.nextLine();
+            System.out.print("Sexo: ");
+            String sexo = sc.next();
+            System.out.print("Idade: ");
+            int idade = sc.nextInt();
+            System.out.print("Peso: ");
+            float peso = sc.nextFloat();
+            System.out.print("Altura: ");
+            float altura = sc.nextFloat();
+
+            float imc = peso / (altura * altura);
+            Pessoa pessoa = new Pessoa(cpf, nome, sexo, idade, peso, altura, imc);
+            pessoas.add(pessoa);
+
+        }
+
+        salvarEmArquivo(pessoas);
+
+        exibirPessoas(pessoas);
+
+        sc.close();
+
+    }
+
+    private static void salvarEmArquivo(List<Pessoa> pessoas) {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            //ESSE TRUE ADICIONA OS DADOS NOVOS AO FINAL DO ARQUIVO, PRESERVANDO OS ANTERIORES
+            outputStream.writeObject(pessoas);
+            System.out.println("Dados salvos em " + FILE_NAME);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+     private static List<Pessoa> carregarDoArquivo() {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            return (List<Pessoa>) inputStream.readObject();
+        } catch (FileNotFoundException e) {
+            System.err.println("Arquivo não encontrado: " + FILE_NAME);
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Erro ao carregar os dados do arquivo binário: " + e.getMessage());
+        }
+        return null;
+    }
+
+    private static void exibirPessoas(List<Pessoa> pessoas) {
+        for (Pessoa pessoa : pessoas) {
+            System.out.println(pessoa);
+        }
+    }
+}
