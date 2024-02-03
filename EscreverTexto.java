c57ff1cf982c3c5afcd586aceb8a717633d30dfc
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-02-03 15:24:28 -0300

    Adicionado uma nova leitura, .csv utilizando split e foreach para imprimir os campos de maneira adequada.

--- EscreverTexto.java
+++ EscreverTexto.java
@@ -8,9 +8,17 @@ public class EscreverTexto {
     public static void main(String[] args) {
         String caminho = "teste.txt";
         String conteudo = "Caio é muito lindo, charmoso e amoroso";
+        String caminho2 = "teste2.txt";
+        String conteudo2 = "A1;A2;" + "\n" + "B1;B2;";
+
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminho))) {
             writer.write(conteudo);
+
+            BufferedWriter writer2 = new BufferedWriter(new FileWriter(caminho2));
+            writer2.write(conteudo2);
+            writer2.close();
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -27,5 +35,28 @@ public class EscreverTexto {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        // DEIXANDO OS CAMPOS A1;A2; \n B1;B2; SEPARADOS: [A1] [A2] [B1] [B2]
+        try(BufferedReader reader = new BufferedReader(new FileReader(caminho2))){
+            String line = "";
+            String resultado = ""; 
+            line = reader.readLine();
+            while (line != null) {
+
+                String partes[] = line.split(";");
+                for (String parte : partes) {
+                    resultado += "[" + parte + "]" + " "; 
+                }
+                //System.out.println(line);
+                line = reader.readLine();
+            } 
+
+            System.out.println(resultado);
+        } catch (IOException e) {
+            System.out.println("Erro de Leitura");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
