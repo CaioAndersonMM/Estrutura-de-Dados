215ab91bc4985b56be910906a806b5bff8795f7e
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-03-11 01:53:03 -0300

    Inferface genérica LinkedList que herda de List genérica

---
+++ UnidadeII/MyLinkedListInterface.java
@@ -0,0 +1,14 @@
+package UnidadeII;
+
+import java.util.List;
+
+public interface MyLinkedListInterface<T> extends List<T>{
+    void addFirst(T value);
+    void addLast(T value);
+
+    T peekFirst();
+    T peekLast();
+
+    T removeFirst();
+    T removeLast();
+}
