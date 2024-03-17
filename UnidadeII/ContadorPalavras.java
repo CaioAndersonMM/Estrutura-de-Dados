fb7e9ed4155630592a5744173ddd8e099da5978f
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-03-17 16:37:03 -0300

    Implementado Serializable para escrita Binária, o programa agora também escreve a lista de Palavra em um arquivo .bin, e a contagem em um arquivo texto.

--- UnidadeII/ContadorPalavras.java
+++ UnidadeII/ContadorPalavras.java
@@ -1,13 +1,18 @@
 package UnidadeII;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 public class ContadorPalavras {
 
-    public static class Letras{
+    public static class Letras implements Serializable {
         private char simbol;
         private int quantidade;
 
@@ -17,17 +22,19 @@ public class ContadorPalavras {
         }
 
     }
-    public static void main(String[] args) {
-        MyLinkedListInterface<Letras> charList  = new MyLinkedList<Letras>();
 
-         try (BufferedReader reader = new BufferedReader(new FileReader(new File("texto.txt")))) {
+    public static void main(String[] args) {
+        MyLinkedListInterface<Letras> charList = new MyLinkedList<Letras>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(
+                "C:\\Users\\caioa\\Documents\\Projetos\\Estrutura de Dados - LISTA\\EstruturaDeDados\\UnidadeII\\texto.txt"))) {
             int charCode;
-            while ((charCode = reader.read()) != -1) { 
+            while ((charCode = reader.read()) != -1) {
                 char character = (char) charCode;
 
                 boolean encontrado = false;
                 for (int i = 0; i < charList.size(); i++) {
-                    Letras letra = charList.get(i); //indices
+                    Letras letra = charList.get(i); // indices
                     if (letra.simbol == character) {
                         letra.quantidade++;
                         encontrado = true;
@@ -45,8 +52,32 @@ public class ContadorPalavras {
         }
 
         for (int i = 0; i < charList.size(); i++) {
-            Letras letra = charList.get(i); //indices
+            Letras letra = charList.get(i); // indices
             System.out.println(letra.simbol + " - " + letra.quantidade);
         }
+
+        try (ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(
+                "C:\\Users\\caioa\\Documents\\Projetos\\Estrutura de Dados - LISTA\\EstruturaDeDados\\UnidadeII\\texto.bin"))) {
+            writer.writeObject(charList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\caioa\\Documents\\Projetos\\Estrutura de Dados - LISTA\\EstruturaDeDados\\UnidadeII\\textoSalvo.txt", false))) {
+            StringBuffer buffer = new StringBuffer();
+            for (int i = 0; i < charList.size(); i++) {
+                Letras letra = charList.get(i);
+                buffer.append(letra.simbol); 
+                buffer.append(": ");
+                buffer.append(letra.quantidade); 
+                buffer.append("\n");
+            }
+
+            writer.write(buffer.toString()); 
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
