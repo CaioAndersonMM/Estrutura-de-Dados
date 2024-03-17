f6aa679186b2db10c482876ec59574c818643e6b
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-03-17 16:32:58 -0300

    REFACTOR: Simplificação de código, um try-catch único para os métodos.

--- UnidadeII/Questão8Pilha.java
+++ UnidadeII/Questão8Pilha.java
@@ -4,54 +4,45 @@ import java.util.Scanner;
 
 public class Questão8Pilha {
     public static void main(String[] args) {
-    String exp;
+        String exp;
 
-    System.out.println("Diga a expressão: ");
-    Scanner sc = new Scanner(System.in);
-    exp = sc.nextLine();
+        System.out.println("Diga a expressão PÓS FIXA: ");
+        Scanner sc = new Scanner(System.in);
+        exp = sc.nextLine();
 
-    MyStackListInterface<String> pilha = new MyStackList<String>(); 
+        MyStackListInterface<String> pilha = new MyStackList<String>();
 
-    String[] pedacos = exp.split("\\s+"); // Dividindo a string pelos espaços em branco
-    
-    for (String pedaco : pedacos) {
-        if (pedaco.equals("+") || pedaco.equals("-")) { //Se for operador
-            try {
-                String ultimo = pilha.pop();
-                String penultimo = pilha.pop();
+        String[] pedacos = exp.split("\\s+"); // Dividindo a string pelos espaços em branco
 
-                if (pedaco.equals("+")) {
-                    int soma =  Integer.parseInt(penultimo) + Integer.parseInt(ultimo);
-                    System.out.println(soma);
-                    String resultado = Integer.toString(soma);
-                    pilha.push(resultado);
-                    System.out.println("empílhado" + resultado);
-                } else{
-                    int sub =  Integer.parseInt(penultimo) - Integer.parseInt(ultimo);
-                    String resultado = Integer.toString(sub);
-                    pilha.push(resultado);
+        try {
+            for (String pedaco : pedacos) {
+                if (pedaco.equals("+") || pedaco.equals("-")) { // Se for operador
+                    String ultimo = pilha.pop();
+                    String penultimo = pilha.pop();
+
+                    if (pedaco.equals("+")) {
+                        int soma = Integer.parseInt(penultimo) + Integer.parseInt(ultimo);
+                        //System.out.println(soma);
+                        String resultado = Integer.toString(soma);
+                        pilha.push(resultado);
+                        //System.out.println("empílhado" + resultado);
+                    } else {
+                        int sub = Integer.parseInt(penultimo) - Integer.parseInt(ultimo);
+                        String resultado = Integer.toString(sub);
+                        pilha.push(resultado);
+                    }
+                } else { // É operando!
+                    pilha.push(pedaco);
                 }
-
-            } catch (MyException e) {
-                e.printStackTrace();
-            }    
-        } else{
-            try {
-                pilha.push(pedaco);
-            } catch (MyException e) {
-                e.printStackTrace();
+                // System.out.println(pedaco);
             }
+            System.out.println("Resultado: " + pilha.peek());
+
+        } catch (MyException e) {
+            e.printStackTrace();
         }
 
-        System.out.println(pedaco);
-    }
+        sc.close();
 
-    try {
-        System.out.println(pilha.peek());
-    } catch (MyException e) {
-        e.printStackTrace();
-    }
-
-    sc.close();
     }
 }
